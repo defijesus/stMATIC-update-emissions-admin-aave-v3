ddf2eaba12c210032ec805f2aa978f810d6fc2fe
defijesus.eth <defijesus@buzzedbearhideout.com>
2022-11-30 12:07:50 +0000

    remove unused code in tests

--- tests/ChangeStmaticEmissionAdmin.t.sol
+++ tests/ChangeStmaticEmissionAdmin.t.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {Test} from 'forge-std/Test.sol';
 import {AaveV3Polygon} from 'aave-address-book/AaveV3Polygon.sol';
 
 import {IEmissionManager} from '../src/interfaces/IEmissionManager.sol';
@@ -11,7 +10,6 @@ import {ChangeEmissionAdminPayload} from '../src/contracts/ChangeStmaticEmission
 
 contract ChangeEmissionAdminTest is BaseTest {
   ChangeEmissionAdminPayload public payload;
-  address constant GUARDIAN = 0xE50c8C619d05ff98b22Adf991F17602C774F785c;
   IEmissionManager constant EMISSION_MANAGER =
     IEmissionManager(0x048f2228D7Bf6776f99aB50cB1b1eaB4D1d4cA73);
 
